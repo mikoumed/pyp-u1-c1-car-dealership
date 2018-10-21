2c821fbd5f69b8b395ca96c4ccad8768f3eb19b7
mikoumed <mikoumed87@gmail.com>
2018-10-21 16:12:31 +0000

    get all tests passing

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,47 @@
+from dealership.vehicles import Car, Truck, Motorcycle
+
 class Contract(object):
-    pass
+    def __init__(self,vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer      
 
 
 class BuyContract(Contract):
+    INTEREST_MULTIPLIERS = {
+            Car : 1.07,
+            Motorcycle : 1.03,
+            Truck : 1.11
+    }
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
 
+    def total_value(self):
+        Discount = 1
+        if self.customer.is_employee():
+            Discount = 0.9
+        return (self.vehicle.sale_price() + (self.INTEREST_MULTIPLIERS[self.vehicle.__class__] * self.monthly_payments * self.vehicle.sale_price() / 100)) * Discount
+    
+    def monthly_value(self):
+        return self.total_value() / self.monthly_payments
 
 class LeaseContract(Contract):
+    
+    LEASE_MULTIPLIERS = {
+            Car : 1.2,
+            Motorcycle : 1,
+            Truck : 1.7,
+    }
+    
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+        Discount = 1
+        if self.customer.is_employee():
+            Discount = 0.9
+        return (self.vehicle.sale_price() + (self.vehicle.sale_price() * self.LEASE_MULTIPLIERS[self.vehicle.__class__] / self.length_in_months)) * Discount
+
+    def monthly_value(self):
+        return self.total_value() / self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,32 @@
 class Vehicle(object):
+    SALE_PRICE_MULTIPLIER = 1
+    PURCHASE_PRICE_MULTIPLIER = 0
+        
     def __init__(self, maker, model, year, base_price, miles):
-        pass
-
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+    def sale_price(self):
+        return self.base_price * self.SALE_PRICE_MULTIPLIER
+        
+    def purchase_price(self):
+        return self.sale_price() - (self.PURCHASE_PRICE_MULTIPLIER * self.miles)
 
 class Car(Vehicle):
-    pass
+    SALE_PRICE_MULTIPLIER = 1.2
+    PURCHASE_PRICE_MULTIPLIER = 0.004
 
-
+        
 class Motorcycle(Vehicle):
-    pass
+    SALE_PRICE_MULTIPLIER = 1.1
+    PURCHASE_PRICE_MULTIPLIER = 0.009
 
 
 class Truck(Vehicle):
-    pass
+    SALE_PRICE_MULTIPLIER = 1.6
+    PURCHASE_PRICE_MULTIPLIER = 0.02
+    
+ 
